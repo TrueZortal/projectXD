0e8e231cb29f76055f8bb120211c710338eed288
Mateusz Buczek <mtsbck@gmail.com>
2022-07-21 18:14:58 +0200

    #Pre-Alpha: separated tests following suggestions, added minions folder to store the hordes of undead

--- minion.rb
+++ minion.rb
@@ -1,16 +1,15 @@
 class Minion
   @@MINION_DATA = {
-    'skeleton': {health: 5, attack: 1, defense: 1}
+    'skeleton': {mana: 1, health: 5, attack: 1, defense: 1}
   }
   attr_accessor :x, :y
   attr_reader :mana, :owner
 
-  def initialize(x: 0, y: 0, mana: 0, owner: '', name: '')
+  def initialize(x: 0, y: 0, owner: '', name: '')
     raise ArgumentError if x.negative? || y.negative?
 
     @x = x
     @y = y
-    @mana = mana
     @owner = owner
     @name = name
   end
--- minions/skeleton.rb
+++ minions/skeleton.rb
@@ -1,10 +1,9 @@
-require_relative 'minion'
+require_relative '../minion'
 
 class RaisedSkeleton < Minion
   attr_accessor :attack, :defense, :health
 
   def place(x: @x, y: @y)
-    @mana = 1
     @attack = @@MINION_DATA[@name.to_sym][:attack]
     @defense = @@MINION_DATA[@name.to_sym][:defense]
     @health = @@MINION_DATA[@name.to_sym][:health]
