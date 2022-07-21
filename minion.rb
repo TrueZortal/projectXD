5fb56d95149fa38e7b122624a08024980469ee48
Mateusz Buczek <mtsbck@gmail.com>
2022-07-21 21:24:38 +0200

    #Pre-Alpha: removed skeleton minion, opting for a class with dictionary for summoned minions, place logic to be included for board as next steps

--- minion.rb
+++ minion.rb
@@ -2,16 +2,20 @@ class Minion
   @@MINION_DATA = {
     'skeleton': {mana: 1, health: 5, attack: 1, defense: 1}
   }
-  attr_accessor :x, :y
-  attr_reader :mana, :owner
+  attr_accessor :x, :y, :attack, :defense, :health
+  attr_reader :mana, :owner, :type
 
-  def initialize(x: 0, y: 0, owner: '', name: '')
-    raise ArgumentError if x.negative? || y.negative?
+  def initialize(x: 0, y: 0, owner: '', type: 'skeleton')
+    raise ArgumentError if x.negative? || y.negative? || !@@MINION_DATA.keys.include?(type.to_sym)
 
     @x = x
     @y = y
     @owner = owner
-    @name = name
+    @type = type
+    @attack = @@MINION_DATA[@type.to_sym][:attack]
+    @defense = @@MINION_DATA[@type.to_sym][:defense]
+    @health = @@MINION_DATA[@type.to_sym][:health]
+
   end
 
   def move(x_y_coordinate_array)
--- minion_test.rb
+++ minion_test.rb
@@ -1,13 +1,11 @@
 require 'minitest/autorun'
 require_relative 'minion'
-require_relative 'skeleton'
 
 class MinionTest < Minitest::Test
   def test_can_create_a_new_minion_object_without_type
       skelly = Minion.new
       assert_equal 0, skelly.x
       assert_equal 0, skelly.y
-      assert_equal 0, skelly.mana
   end
 
   def test_cant_create_a_new_minion_with_a_negative_coordinate_x
@@ -23,7 +21,7 @@ class MinionTest < Minitest::Test
   end
 
   def test_minion_can_be_placed_anywhere_if_not_connected_to_a_board
-    skelly = Minion.new(x: 7, y:1000)
+    skelly = Minion.new(x: 7, y: 1000)
     assert_equal 1000, skelly.y
   end
 
@@ -31,20 +29,18 @@ class MinionTest < Minitest::Test
     skelly = Minion.new(owner: 'Mateusz')
     assert_equal 'Mateusz', skelly.owner
   end
-end
 
-class RaisedSkeletonTest < Minitest::Test
-  def test_skeleton_can_be_placed_anywhere_if_not_connected_to_a_board
-    skelly = RaisedSkeleton.new(x: 1, y: 666)
-    assert_equal 666, skelly.y
-  end
-
-  def test_skeleton_has_correct_default_values_when_created
+  def test_a_skeleton_minion_has_correct_default_values_when_created
     # skip
-    skelly = RaisedSkeleton.new(name: 'skeleton')
-    skelly.place
+    skelly = Minion.new(type: 'skeleton')
     assert_equal 1, skelly.attack
     assert_equal 1, skelly.defense
     assert_equal 5, skelly.health
+  end
+
+  def test_cant_create_a_minion_of_non_existent_type
+    assert_raises(ArgumentError) do
+      skelly = Minion.new(type: 'tomato')
+    end
   end
 end
