e4daafea933be3fa9ba3a100cb176873c3429f77
Mateusz Buczek <mtsbck@gmail.com>
2022-07-25 20:17:53 +0200

    #Pre-Alpha field test separated from board test file

--- board_test.rb
+++ board_test.rb
@@ -5,24 +5,6 @@ require_relative 'board'
 require_relative 'minion'
 require_relative 'field'
 
-class FieldTest < Minitest::Test
-  def test_can_create_a_new_field_with_default_values
-    test = Field.new
-    assert_equal 0, test.x
-    assert_equal 0, test.y
-    assert_equal 'empty', test.status
-    assert_equal '', test.terrain
-    assert_equal '', test.occupant
-    assert_equal '', test.obstacle
-  end
-
-  def test_can_create_a_new_field_with_custom_coordinates
-    test = Field.new(x: 3, y: 4)
-    assert_equal 3, test.x
-    assert_equal 4, test.y
-  end
-end
-
 class BoardTest < Minitest::Test
   def test_cant_create_a_board_without_an_argument
     assert_raises(ArgumentError) do
